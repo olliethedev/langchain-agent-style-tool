d6c4b14f9dd2f915672a0ef355873ba636328f3c
olliethedev <3martynov@gmail.com>
2023-06-01 10:23:18 -0400

    chore: improvements

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import { initializeAgentExecutorWithOptions } from "langchain/agents";
 import StyleTool from "./StyleTool";
 
 // Define the URL and input message as constants for better readability
-const URL = "https://www.answera.ai/";
+const URL = "https://www.airbnb.ca/";
 
-const ELEMENT_DESCRIPTION = "Hero section with an image, title, subtitle, and a button"
+const ELEMENT_DESCRIPTION = "a design system - with a small set of reusible core components and styles that match the brand style";
 
-const inputMessage = `With the aid of the StyleExtractorTool, our task is to construct an HTML page featuring a ${ELEMENT_DESCRIPTION}. The style of this element should be inspired by the aesthetics of ${URL}. We will restrict the use of the tool to only one instance. Your output should exclusively consist of valid HTML and CSS code, and any other information or output should be omitted. Please refrain from utilizing CSS variables such as var(--box-shadow). For text content, Lorem Ipsum should be used. For image URLs, please use the following placeholder: http://via.placeholder.com/640x360. It's crucial to ensure a high contrast between the background and foreground to maintain legibility.
+const inputMessage = `With the aid of the StyleExtractorTool, our task is to construct an HTML page featuring a ${ELEMENT_DESCRIPTION}. The style of this element should be inspired by the aesthetics of ${URL}. We will restrict the use of the tool to only one instance. Your output should exclusively consist of valid HTML and CSS code, and any other information or output should be omitted. Please refrain from utilizing CSS variables such as var(--box-shadow). And refrain from linking to external style sheets. For text content, Lorem Ipsum should be used. For image URLs, please use the following placeholder: http://via.placeholder.com/640x360. It's crucial to ensure a high contrast between the background and foreground to maintain legibility.
 `;
 
 async function run() {
@@ -17,7 +17,7 @@ async function run() {
         modelName:"gpt-3.5-turbo",
         temperature: 0.4,
         verbose: true,
-        timeout: 2 * 60 * 1000
+        timeout: 5 * 60 * 1000
     });
 
     // Add StyleTool to the tools array
@@ -34,7 +34,9 @@ async function run() {
     // Call the executor with the input message
     const result = await executor.call({ input: inputMessage });
 
+
     console.log(`Got output ${ result.output }`);
+
 }
 
 // Run the async function
